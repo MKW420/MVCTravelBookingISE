1109d6eae54333250e293c74671d1ac49ae98f98
MKW420 <keitumetseshongwe@gmail.com>
2022-08-25 00:33:58 +0200

    Controllers added

---
+++ MVCTravelBookingISE/Controllers/TravellerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCTravelBookingISE.Controllers
+{
+    public class TravellerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
