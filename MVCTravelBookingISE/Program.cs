543a666be9921e2c3657cee1cc958e380b0f9bc0
MKW420 <keitumetseshongwe@gmail.com>
2022-08-30 22:47:37 +0200

    M/DevDb

--- MVCTravelBookingISE/Program.cs
+++ MVCTravelBookingISE/Program.cs
@@ -3,12 +3,36 @@ using MVCTravelBookingISE.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//var host = CreateHostBuilder(args).Build();
+
+//using (var scope = host.Services.CreateScope())
+//{
+ //   var services = scope.ServiceProvider;
+
+  // try
+  // {
+  //     var context = services.GetRequiredService<AppDbContext>();
+   //   context.Database.Migrate();
+
+  // }
+  // catch(Exception ex)
+  // {
+//       var logger = services.GetRequiredService<ILogger<Program>>();
+//        logger.LogError(ex, "An error occured creaing the DB.");
+ ///   }
+//}
+//host.Run();
+ 
+//public static IHostBuilder CreateHostBuilder(string[]args)=>
+  //  Host.C
+
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 //DB context configiration
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultconnectionString")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 
 var app = builder.Build();
 
